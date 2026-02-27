ec22391b839dd697703171a02be0dcc476da30f1
Prithaviraj <prithvirajs@hire4higherconsulting.com>
2026-02-27 15:34:28 +0530

    fix: use vite-plugin-node-polyfills to resolve plotly.js buffer error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills(),
+  ],
   server: {
     port: 5173,
-  },
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-    },
-  },
-  optimizeDeps: {
-    include: ['buffer'],
   },
 })
